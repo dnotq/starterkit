a24adec6a071e9a286771e95568fc901bd5ad76b
Matthew Hagerty <matthew@dnotq.io>
2021-07-02 15:40:44 -0400

    Added release/debug build parameter detection to windows batch file.

--- build_win.bat
+++ build_win.bat
@@ -1,10 +1,22 @@
 @echo off
 REM Starter Kit Windows Build.
 REM Requires Visual Studio with CMake.
+
+REM Settings if CMake is not doing the right thing.
 REM cmake -G "Visual Studio 16 2019" -A Win32
 REM cmake -G "Visual Studio 16 2019" -A x64
+REM cmake -E chdir build_win cmake -G "Visual Studio 16 2019" -A Win32 ..
 
-REM cmake -E chdir build cmake -G "Visual Studio 16 2019" -A Win32 ..
 cmake -E make_directory build_win
 cmake -E chdir build_win cmake ..
+
+REM Check for a command-line config parameter (Release or Debug)
+if [%1]==[] goto no_config_parm
+cmake --build build_win --config %1
+goto done
+:no_config_parm
+echo.
+echo Pass a "Release" or "Debug" parameter to specify the build type.
+echo.
 cmake --build build_win
+:done
