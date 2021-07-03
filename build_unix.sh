33a04c546f12be2152e68a889198c4a655ec3dbe
Matthew Hagerty <matthew@dnotq.io>
2021-07-02 21:11:16 -0400

    Added support for debug and release builds. Updated compiler build options for the build types.

--- build_unix.sh
+++ build_unix.sh
@@ -1,9 +1,21 @@
 #!bash
 
-export CC=clang
-export CXX=clang++
-#export CC=gcc
-#export CXX=g++
+#export CC=clang
+#export CXX=clang++
+export CC=gcc
+export CXX=g++
 cmake -E make_directory build_unix
-cmake -E chdir build_unix cmake -G "MinGW Makefiles" ..
-cmake --build build_unix
+
+# Check for a command-line config parameter (Release or Debug)
+if [ -z "$1" ] ; then
+   echo
+   echo "You can pass a \"Release\" or \"Debug\" parameter to specify the build type."
+   echo
+   cmake -E chdir build_unix cmake -G "MinGW Makefiles" ..
+   cmake --build build_unix
+else
+   cmake -E chdir build_unix cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=$1 ..
+   cmake --build build_unix --config $1
+fi
+
+echo "END OF LINE."
--- build_win.bat
+++ build_win.bat
@@ -8,15 +8,19 @@ REM cmake -G "Visual Studio 16 2019" -A x64
 REM cmake -E chdir build_win cmake -G "Visual Studio 16 2019" -A Win32 ..
 
 cmake -E make_directory build_win
-cmake -E chdir build_win cmake ..
 
 REM Check for a command-line config parameter (Release or Debug)
 if [%1]==[] goto no_config_parm
+cmake -E chdir build_win cmake -DCMAKE_BUILD_TYPE=%1 ..
 cmake --build build_win --config %1
 goto done
+
 :no_config_parm
 echo.
-echo Pass a "Release" or "Debug" parameter to specify the build type.
+echo You can pass a "Release" or "Debug" parameter to specify the build type.
 echo.
+cmake -E chdir build_win cmake ..
 cmake --build build_win
+
 :done
+echo END OF LINE.
